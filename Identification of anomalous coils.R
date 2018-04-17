361b5538f58942fc4248870f98ca724bb1fc56dc
Sherry Yang <yangxiaoli1994@gmail.com>
2018-04-17 14:33:32 +0200

    first version of Gaussian

---
+++ Identification of anomalous coils.R
@@ -0,0 +1,45 @@
+# identify if a coil is somehow different from existing observations.
+# Authr: Xiaoli Yang
+# Date: 2018
+library(stats)
+library(ggplot2)
+library(plotrix)
+library(fitdistrplus)
+library(lmtest)
+library(xlsx)
+library(MASS)
+library(mclust, quietly=TRUE) #is used to fit Gaussian mixture models
+
+
+
+Sys.setlocale("LC_ALL","German")
+
+### Input data
+path ="D:/ubuntu/WZL-2018/Automobil_training_data/"
+filename = "Datenbasis_Gefiltert_zusammengefuehrt.csv"
+setwd(path)
+database <- read.csv(filename, header=TRUE,encoding='UTF-8',sep=",")
+# to observe the basic info about database
+names(database)
+dim(database)
+
+### Distribution analysis of `Banddicke2`
+
+## first remove the duplication
+dfm_B2 = database[!duplicated(database[,'Banddicke2']),]
+#TODO: draw distplot_gbanddicke
+
+## second: For a quantitative analysis, mean, median, and std values are analyzed.
+#frequency
+frequencies <- list(100,50,10,1)
+aggfuncs <- list('mean','median','std')
+
+# mixture of gaussian
+fit = Mclust(dfm_B2[,'Banddicke2'], G=4, model="V")
+summary(fit)
+plot(fit, what="density", main="", xlab="Velocity (Mm/s)")
+rug(dfm_B2[,'Banddicke2'])
+
+
+
+
